2f0cc518c3d0e347ab63472bcd0d7568e24daeb3
Nachosky12 <143748140+Nachosky12@users.noreply.github.com>
2024-11-26 14:51:42 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
+
+import Home from "./components/Home/Home";
+
+import About from "./components/About/About";
+import ContactPage from "./components/Contact/Contact";
+import Products from "./components/Products/Products";
+import ItemDetail from "./components/Itemdetail/ItemDetail";
+import ProductsCategory from "./components/Category/Category";
+
+
+function App(){
+    return(
+        <>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path='/products' element={<Products />} />
+                {/*<Route path='/category/:categoryId' element={<ProductsCategory />} />
+                <Route path='/products/:productId' element={<ItemDetail />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<ContactPage />} />*/}
+            </Routes>
+            <Footer/>
+        </BrowserRouter>  
+        </>
+    )
+};
+
+export default App;
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,9 @@
+import ItemList from "../ItemList/ItemList"
+
+export default function Products(){    
+    return(
+        <div className="container mx-auto">
+            <ItemList />
+        </div>   
+    )
+}
